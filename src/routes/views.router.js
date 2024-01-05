c38928f8db6514142bb5a65c9d29c1a64fee7543
dcaceres <dcaceres@asjservicios.com>
2024-01-04 21:40:15 -0300

    fix ruta inicio

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -5,12 +5,12 @@ import { ProductModel}  from "../services/dao/db/models/product.model.js";
 
 const router = Router();
 
-router.get('/products',async(req,res)=>{
+router.get('/',async(req,res)=>{
     let page = parseInt(req.query.page);
     if (!page) page = 1;
     let result = await ProductModel.paginate({}, { page, limit: 5, lean: true });
-    result.prevLink = result.hasPrevPage ? `http://localhost:8080/products?page=${result.prevPage}` : '';
-    result.nextLink = result.hasNextPage ? `http://localhost:8080/products?page=${result.nextPage}` : '';
+    result.prevLink = result.hasPrevPage ? `http://localhost:8080/?page=${result.prevPage}` : '';
+    result.nextLink = result.hasNextPage ? `http://localhost:8080/?page=${result.nextPage}` : '';
     result.isValid = !(page <= 0 || page > result.totalPages)
     res.render('products',result)
 })
